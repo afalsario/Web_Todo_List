563d7d1e9a03c3f75459c9cceab2f9d7193082e7
Ashley <a_falsario@yahoo.com>
2014-06-25 11:10:08 -0500

    fixed indentation, working on css

--- public/new_todo.php
+++ public/new_todo.php
@@ -1,34 +1,42 @@
-<?
-	class InvalidInputException extends Exception{}
+<?php
 
-	// define('FILENAME', 'new.txt');
-	$file_name = 'new.txt';
-	$items = [];
-	$errorMessage = '';
+$items_per_page = 12;
 
-	//require class file
-	require('classes/filestore.php');
-	$todo = new Filestore($file_name);
+class InvalidInputException extends Exception{}
 
-	//load file with list
-	$items = $todo->read();
+function getOffset($items_per_page)
+{
+	$page = isset($_GET['page'])? $_GET['page'] : 1;
+	return ($page - 1) * $items_per_page;
+}
 
+//-------------1. Establish DB Connection
+$dbc = new PDO('mysql:host=127.0.0.1;dbname=todo_db', 'ashley', 'password');
 
+// Tell PDO to throw exceptions on error
+$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 try
 {
-	//if the post is not empty, add item to the list array
+//-------------2. Check if something was posted
 	if(isset($_POST['new_item']))
 	{
-		if(strlen($_POST['new_item']) != 0 && (strlen($_POST['new_item'])) <= 240)
+		//-------------a. Is item being added? => Add todo!
+		if(isset($_POST['new_item']) && (strlen($_POST['new_item']) != 0 && (strlen($_POST['new_item'])) <= 240))
 		{
-		$item = trim($_POST['new_item']);
-		array_push($items, $item);
+			$stmt = $dbc->prepare('INSERT INTO todo_list (todo) VALUES (:todo)');
+		    $stmt->bindValue(':todo', htmlspecialchars(strip_tags($_POST['new_item'])), PDO::PARAM_STR);
+		    $stmt->execute();
 		}
 		else
 		{
 			throw new InvalidInputException("Error! Please enter a todo longer than 0 characters and less than 240");
 		}
+	}
+	//-------------b. Is item being removed? => Remove it! As a post
+	if(isset($_POST['remove_id']))
+	{
+		$dbc->query('DELETE FROM todo_list WHERE id = ' . $_POST['remove_id']);
 	}
 }
 catch (InvalidInputException $e)
@@ -36,81 +44,96 @@ catch (InvalidInputException $e)
 	$e->getMessage();
 }
 
-	//if the user clicks the link to remove and item, remove item and reindex the array
-	if(isset($_GET['index']))
-	{
-		$index = $_GET['index'];
-		unset($items[$index]);
-		$items = array_values($items);
-	}
+//-------------3. Query DB for total todo count.
+$count = $dbc->query('SELECT count(*) FROM todo_list')->fetchColumn();
+//divides the count by the items/page to determine total pages and show the proper pagination links
+$numPages = ceil($count / $items_per_page);
 
-	if(isset($_FILES['file1']) && ($_FILES['file1']['type'] !== 'text/plain'))
-	{
-		$errorMessage = "ERROR: Please use a plain text file";
-	}
-	elseif(count($_FILES) > 0 && $_FILES['file1']['error'] == 0)
-	{
-		//set destination for uploaded files
-		$upload_dir = '/vagrant/sites/todo.dev/public/uploads/';
-		//get the file name using basename
-		$filename = basename($_FILES['file1']['name']);
-		// Create the saved filename using the file's original name and our upload directory
-    	$saved_filename = $upload_dir . $filename;
-    	// Move the file from the temp location to our uploads directory
-    	move_uploaded_file($_FILES['file1']['tmp_name'], $saved_filename);
+//-------------4. Determine pagination values.
+$page = isset($_GET['page'])?$_GET['page']: 1;
+$previous_page = $page - 1;
+$next_page = $page + 1;
 
-    	$add_new_file = new Filestore($saved_filename);
-    	$new_items = $add_new_file->read();
-    	$items = array_merge($items, $new_items);
-    	$todo->write($items);
-	}
-	//save/overwrite the defined file with added items
-	$todo->write($items);
+//preparing data from the table so that it shows a limit of items and an offset for pagination
+$query = 'SELECT * FROM todo_list LIMIT :limit OFFSET :offset';
+$stmt = $dbc->prepare($query);
+$stmt->bindValue(':limit', $items_per_page, PDO::PARAM_INT);
+$stmt->bindValue(':offset', getOffset($items_per_page), PDO::PARAM_INT);
+$stmt->execute();
+
+//-------------5. Query for todos on current page.
+$list = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <html>
 <head>
 	<title>TODO List</title>
-	<!-- <link rel="stylesheet" href="/css/todo_list.css"> -->
-<!-- 	<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
- --></head>
+    <link rel="stylesheet" href="/css/.css">
+</head>
 <body>
-<? if(isset($e)): ?>
-<p style="color:red"><?= $e->getMessage(); ?></p>
-<? endif; ?>
-	<header>
-		<!-- <img src="/img/todo1.png"> -->
-	</header>
-	<div class="container">
-		<table>
+
+    <? if(isset($e)): ?>
+        <p style="color:red"> <?= $e->getMessage(); ?> </p>
+    <? endif; ?>
+
+    <div class="container">
+		<table class="table table-striped">
 			<ul>
-			<!-- display each item in the array -->
-			<? foreach($items as $key => $item): ?>
-				<tr>
-				<td><li> <?= htmlspecialchars(strip_tags($item)) . " "; ?></td> <td><?="<a href=\"todo_list.php?index={$key}\">Remove Item</a>";?></li></td>
-				</tr>
-			<? endforeach; ?>
+    			<!-- display each item in the table -->
+    			<? foreach($list as $item): ?>
+    				<tr>
+        				<td><?= $item['id']; ?></td>
+        				<td><?= $item['todo'] ?> </td>
+        				<td><button class="btn-remove" data-todo="<?= $item['id']; ?>">Remove</button></td>
+    				</tr>
+    			<? endforeach; ?>
 			</ul>
 		</table>
 	</div>
 
-	<!-- form to post new items to the existing array -->
-	<footer>
-		<form method="POST" action="/todo_list.php">
-			<p>
-				<label for="new_item">New Item:</label>
-				<input type="text" id="new_item" name="new_item" autofocus="autofocus">
-				<input type="submit" value="Add">
-			</p>
-		</form>
-		<h3>Upload File</h3>
-		<? if (!empty($errorMessage)): ?>
-		    <p> <?= $errorMessage; ?> </p>
-		<? endif; ?>
-		<form method="POST" enctype="multipart/form-data">
-			<input type="file" id="file1" name="file1">
-			<input type="submit" value="Upload">
-		</form>
-	</footer>
-</body>
+	<ul class="pager">
+	        <? if(getOFFSET($items_per_page) !== 0): ?>
+	    <li><?= "<a href=\"?page={$previous_page}\">Previous</a>"; ?></li>
+	    	<? endif; ?>
+	    	<? if($numPages > $page): ?>
+	    <li><?= "<a href=\"?page={$next_page}\">"; ?>Next</a></li>
+	    	<? endif; ?>
+    </ul>
 
+	<!-- form to post new items to the database-->
+	<form method="POST" action="new_todo.php">
+		<p>
+			<label for="new_item">New Item:</label>
+			<input type="text" id="new_item" name="new_item" autofocus="autofocus" value="">
+			<button>Add Item</button>
+		</p>
+	</form>
+
+	<form method="POST" action="new_todo.php" id="remove_form">
+		<input type="hidden" name="remove_id" id="remove_id"  value="">
+	</form>
+
+    <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script>
+        $('.btn-remove').click(function() {
+            var todoId = $(this).data('todo');
+            if(confirm('Are you sure you want to remove item ' + todoId + '?'))
+            {
+        	    $('#remove_id').val(todoId);
+        	    $('#remove_form').submit();
+        	}
+        });
+    </script>
+
+</body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
