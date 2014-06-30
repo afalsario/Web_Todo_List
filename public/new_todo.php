ac85a1f56510ca91c1b186047e0ff0f4ba6ac783
Ashley <a_falsario@yahoo.com>
2014-06-30 15:01:20 -0500

    added input in form and modified spacing

--- public/new_todo.php
+++ public/new_todo.php
@@ -1,13 +1,12 @@
 <?php
-
-$items_per_page = 2;
+$items_per_page = 5;
 
 class InvalidInputException extends Exception{}
 
 function getOffset($items_per_page)
 {
-	$page = isset($_GET['page'])? $_GET['page'] : 1;
-	return ($page - 1) * $items_per_page;
+    $page = isset($_GET['page'])? $_GET['page'] : 1;
+    return ($page - 1) * $items_per_page;
 }
 
 //-------------1. Establish DB Connection
@@ -19,29 +18,29 @@ $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 try
 {
 //-------------2. Check if something was posted
-	if(isset($_POST['new_item']))
-	{
-		//-------------a. Is item being added? => Add todo!
-		if((strlen($_POST['new_item']) != 0 && (strlen($_POST['new_item'])) <= 240))
-		{
-			$stmt = $dbc->prepare('INSERT INTO todo_list (todo) VALUES (:todo)');
-		    $stmt->bindValue(':todo', htmlspecialchars(strip_tags($_POST['new_item'])), PDO::PARAM_STR);
-		    $stmt->execute();
-		}
-		else
-		{
-			throw new InvalidInputException("Error! Please enter a todo longer than 0 characters and less than 240");
-		}
-	}
-	//-------------b. Is item being removed? => Remove it! As a post
-	if(isset($_POST['remove_id']))
-	{
-		$dbc->query('DELETE FROM todo_list WHERE id = ' . $_POST['remove_id']);
-	}
+    if(isset($_POST['new_item']))
+    {
+        //-------------a. Is item being added? => Add todo!
+        if((strlen($_POST['new_item']) != 0 && (strlen($_POST['new_item'])) <= 240))
+        {
+            $stmt = $dbc->prepare('INSERT INTO todo_list (todo) VALUES (:todo)');
+            $stmt->bindValue(':todo', htmlspecialchars(strip_tags($_POST['new_item'])), PDO::PARAM_STR);
+            $stmt->execute();
+        }
+        else
+        {
+            throw new InvalidInputException("Error! Please enter a todo longer than 0 characters and less than 240");
+        }
+    }
+    //-------------b. Is item being removed? => Remove it! As a post
+    if(isset($_POST['remove_id']))
+    {
+        $dbc->query('DELETE FROM todo_list WHERE id = ' . $_POST['remove_id']);
+    }
 }
 catch (InvalidInputException $e)
 {
-	$e->getMessage();
+    $e->getMessage();
 }
 
  if(isset($_POST['reset']))
@@ -71,7 +70,7 @@ $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <html>
 <head>
-	<title>TODO List</title>
+    <title>TODO List</title>
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/todo_list.css">
 </head>
@@ -82,45 +81,46 @@ $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <? endif; ?>
 
     <div class="container">
-		<table class="table table-striped">
-			<ul>
-    			<!-- display each item in the table -->
-    			<? foreach($list as $item): ?>
-    				<tr>
-        				<td><?= $item['id']; ?></td>
-        				<td><?= $item['todo'] ?> </td>
-        				<td><button class="btn btn-danger btn-remove" data-todo="<?= $item['id']; ?>">Remove</button></td>
-    				</tr>
-    			<? endforeach; ?>
-			</ul>
-		</table>
-	</div>
-
-	<ul class="pager">
-	        <? if(getOFFSET($items_per_page) !== 0): ?>
-	    <li><?= "<a href=\"?page={$previous_page}\">Previous</a>"; ?></li>
-	    	<? endif; ?>
-	    	<? if($numPages > $page): ?>
-	    <li><?= "<a href=\"?page={$next_page}\">"; ?>Next</a></li>
-	    	<? endif; ?>
-    </ul>
-
-	<!-- form to post new items to the database-->
-	<div id="add_form">
-        <form method="POST" action="#">
-    		<p>
-    			<label for="new_item">New Item:</label>
-    			<input type="text" id="new_item" name="new_item" autofocus="autofocus" value="">
-    			<button class="btn btn-primary">Add Item</button>
-    		</p>
-    	</form>
+        <table class="table table-striped">
+            <ul>
+                <!-- display each item in the table -->
+                <? foreach($list as $item): ?>
+                    <tr>
+                        <td><?= $item['id']; ?></td>
+                        <td><?= $item['todo'] ?> </td>
+                        <td><button class="btn btn-danger btn-remove" data-todo="<?= $item['id']; ?>">Remove</button></td>
+                    </tr>
+                <? endforeach; ?>
+            </ul>
+        </table>
     </div>
 
-	<form method="POST" action="#" id="remove_form">
-		<input type="hidden" name="remove_id" id="remove_id"  value="">
-	</form>
+    <ul class="pager">
+            <? if(getOFFSET($items_per_page) !== 0): ?>
+        <li><?= "<a href=\"?page={$previous_page}\">Previous</a>"; ?></li>
+            <? endif; ?>
+            <? if($numPages > $page): ?>
+        <li><?= "<a href=\"?page={$next_page}\">"; ?>Next</a></li>
+            <? endif; ?>
+    </ul>
+
+    <!-- form to post new items to the database-->
+    <div id="add_form">
+        <form method="POST" action="#">
+            <p>
+                <label for="new_item">New Item:</label>
+                <input type="text" id="new_item" name="new_item" autofocus="autofocus" value="">
+                <button class="btn btn-primary">Add Item</button>
+            </p>
+        </form>
+    </div>
+
+    <form method="POST" action="#" id="remove_form">
+        <input type="hidden" name="remove_id" id="remove_id"  value="">
+    </form>
 
     <form method="POST" action="/new_todo.php" id="reset_form">
+        <input type="hidden" name="reset" value="true">
         <button name="reset" class="btn-reset btn btn-warning">Reset</button>
     </form>
 
@@ -130,29 +130,19 @@ $list = $stmt->fetchAll(PDO::FETCH_ASSOC);
             var todoId = $(this).data('todo');
             if(confirm('Are you sure you want to remove item ' + todoId + '?'))
             {
-        	    $('#remove_id').val(todoId);
-        	    $('#remove_form').submit();
-        	}
+                $('#remove_id').val(todoId);
+                $('#remove_form').submit();
+            }
         });
 
-        // $('.btn-reset').click(function() {
-        //     if(confirm('Are you sure you want to reset?'))
-        //     {
-        //         $('#reset_form').submit();
-        //     }
-        // });
+        $('.btn-reset').click(function(e) {
+            e.preventDefault();
+            if(confirm('Are you sure you want to reset?'))
+            {
+                $('#reset_form').submit();
+            }
+        });
     </script>
 
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
